e21138b62acc531311653c9a97e110e534b6a7b1
yujeonghyeon <dbdb1114@naver.com>
2023-07-05 17:25:57 +0900

    기본 컨트롤러 작성

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+
+
+}
--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View를 리턴하겠다.
 public class IndexController {
@@ -14,6 +15,37 @@ public class IndexController {
         // 머스테치 기본경로 src/main/resources/
         // View Resolver : templates(prefix), .mustache(seffix) 생략가능.
         return "/index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "/admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "/manager";
+    }
+
+    // 스프링 시큐리티 해당주소를 가져감.
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "/login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "/join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨.";
     }
 
 }
